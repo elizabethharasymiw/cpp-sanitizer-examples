6d713a98948676cdc815735b9fe6bf5f4d4712b9
elizabethharasymiw <elizabeth.harasymiw@gmail.com>
2024-06-15 13:18:53 -0500

    Add second data race example in TSan examples

---
+++ TSan-ThreadSanitizer-Examples/second-data-race-example/main.c
@@ -0,0 +1,26 @@
+/*
+ * Written to show off how ThreadSanitizer displays data races it finds
+ */
+
+#include <pthread.h>
+
+int Global;
+
+void* Thread1(void *x){
+  Global++;
+  return NULL;
+}
+
+void* Thread2(void *x){
+  Global--;
+  return NULL;
+}
+
+int main(){
+  pthread_t t[2];
+  pthread_create(&t[0], NULL, Thread1, NULL);
+  pthread_create(&t[1], NULL, Thread2, NULL);
+  pthread_join(t[0], NULL);
+  pthread_join(t[1], NULL);
+  return 0;
+}
